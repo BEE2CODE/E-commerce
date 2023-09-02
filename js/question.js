077c50fc58fba5c98bd0cf1f40519e679788d411
BEE2CODE <70761501+BEE2CODE@users.noreply.github.com>
2023-09-02 15:34:51 +0100

    E-commerce

---
+++ js/question.js
@@ -0,0 +1,552 @@
+let ques = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is making web Standard?",
+        answer: "The World Wide Web Consortium" ,
+        options: [
+            "Google",
+            "Microsoft",
+            "Mozilla",
+            "The World Wide Web Consortium"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many HTML file can we have on our project",
+        answer:  "As many has required",
+        options: [
+            "Two",
+            "Not more than one",
+            "Not more than three",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the correct HTML element for inserting a line break?",
+        answer:  "<br>",
+        options: [
+            "<br>",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the correct html extension",
+        answer:  ".html",
+        options: [
+            ".js",
+            ".css",
+            ".html",
+            ".asset"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is used to add tooltip",
+        answer:  "Title",
+        options: [
+            "Paragraph tag",
+            "Section tag",
+            "Header",
+            "Title"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is used to set width and font-size",
+        answer:  "Px and %",
+        options: [
+            "Px and %",
+            "*",
+            "+",
+            "-"
+        ]
+    },
+    {
+        numb: 8,
+        question: "_______ is used for image tag",
+        answer:  "<img src>",
+        options: [
+            "<p></p>",
+            "<section></section>",
+            "<img src>",
+            "<a>"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the correct html self closing tag",
+        answer:  "Input",
+        options: [
+            "Paragraph",
+            "section",
+            "header",
+            "Input"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup Language9",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What is <a> used for",
+        answer:  "It is used for linking html pages",
+        options: [
+            "To insert image on the website",
+            "It can also be used to display input tag",
+            "It is can only be used on header tag",
+            "Are used to write a paragraph of text"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What are HTML Attributes?",
+        answer:  "Attributes are the properties that can be added to an HTML tag",
+        options: [
+            "Hyper",
+            "Are marquee used for scrolling text on a web page",
+            "Are used to write a paragraph of text",
+            "This are tag used to define large quoted sections"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What is the difference between HTML and CSS?",
+        answer:  "HTML is used to create the structure and content of a web page, while CSS is used to define the appearance and layout of the page",
+        options: [
+            "They are both used to add javascript",
+            "They both used for same purpose",
+            "This is the functionality part of web",
+            "HTML is used to create the structure and content of a web page, while CSS is used to define the appearance and layout of the page"
+        ]
+    },
+    {
+        numb: 14,
+        question: "How do you separate a section of texts in HTML?",
+        answer:  "Using <br>",
+        options: [
+            "Hyper Tool Markup4 Language",
+            "Using attributes",
+            "Home Tool Markup Language",
+            "Using <br>"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Define the list types in HTML?",
+        answer:  "ol, ul",
+        options: [
+            "Hyper Tool Markup5 Language",
+            "Hyper Text Markup Language",
+            "ol, ul",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 16,
+        question: "How do you align list elements in an HTML file?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup6 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Differentiate between an Ordered list and an Unordered list?",
+        answer:  "An unordered list uses <ul> </ul> and ordered <ol> </ol>tags",
+        options: [
+            "An unordered list uses <ul> </ul> tags",
+            "An unordered list uses <ul> </ul> and ordered <ol> </ol>tags",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 18,
+        question: "How do you create a hyperlink in HTML?",
+        answer:  "We use the anchor tag <a> to create a hyperlink in HTML ",
+        options: [
+            "Hyper Tool Markup8 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What is semantic HTML?",
+        answer:  " It is the use of HTML markup to reinforce the semantics or meaning of the conten",
+        options: [
+            "It is the use of HTML markup to reinforce the semantics or meaning of the conten",
+            "Hyper Text Markup Language",
+            "This is arrangement of tags on the webpage",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What would happen if there is no text between the HTML tags?",
+        answer:  "nothing will appear on the screen",
+        options: [
+            "nothing will appear on the screen",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool2 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 22,
+        question: "How do you create nested web pages in HTML?",
+        answer:  "Nested web pages basically mean a webpage within a webpage",
+        options: [
+            "Nested web pages basically mean a webpage within a webpage",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 23,
+        question: "What are the different types of headings in HTML?",
+        answer:  "Size",
+        options: [
+            "shape",
+            "fix",
+            "color",
+            "background-color"
+        ]
+    },
+    {
+        numb: 24,
+        question: "What is the alt attribute in HTML?",
+        answer:  "alternative",
+        options: [
+            "section",
+            "alternative",
+            "paragrahy",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 25,
+        question: "How do you add colour to the text in HTML?",
+        answer:  "<style = color: red;",
+        options: [
+            "font-sixe",
+            "style = color: red;",
+            "position:relative",
+            "align-item:center"
+        ]
+    },
+    {
+        numb: 26,
+        question: "What is HTML used for",
+        answer:  "Create the structure of a webpage",
+        options: [
+            "Create the structure of a webpage",
+            "For styling webpage",
+            "Make food",
+            "Is for adding functionality to webpage"
+        ]
+    },
+    {
+        numb: 27,
+        question: "How many HTML file can we have on our project",
+        answer:  "As many has required",
+        options: [
+            "Two",
+            "Not more than one",
+            "Not more than three",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 28,
+        question: "How do you separate a section of texts in HTML?",
+        answer:  "Using <br>",
+        options: [
+            "Hyper Tool Markup4 Language",
+            "Using attributes",
+            "Home Tool Markup Language",
+            "Using <br>"
+        ]
+    },
+    {
+        numb: 29,
+        question: "Define the list types in HTML?",
+        answer:  "ol, ul",
+        options: [
+            "Hyper Tool Markup5 Language",
+            "Hyper Text Markup Language",
+            "ol, ul",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 30,
+        question: "How do you align list elements in an HTML file?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool Markup6 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 31,
+        question: "Differentiate between an Ordered list and an Unordered list?",
+        answer:  "An unordered list uses <ul> </ul> and ordered <ol> </ol>tags",
+        options: [
+            "An unordered list uses <ul> </ul> tags",
+            "An unordered list uses <ul> </ul> and ordered <ol> </ol>tags",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 32,
+        question: "How do you create a hyperlink in HTML?",
+        answer:  "We use the anchor tag <a> to create a hyperlink in HTML ",
+        options: [
+            "Hyper Tool Markup8 Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 33,
+        question: "What is semantic HTML?",
+        answer:  " It is the use of HTML markup to reinforce the semantics or meaning of the conten",
+        options: [
+            "It is the use of HTML markup to reinforce the semantics or meaning of the conten",
+            "Hyper Text Markup Language",
+            "This is arrangement of tags on the webpage",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 34,
+        question: "What would happen if there is no text between the HTML tags?",
+        answer:  "nothing will appear on the screen",
+        options: [
+            "nothing will appear on the screen",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 35,
+        question: "What does HTML stand for?",
+        answer:  "Hyper Text Markup Language",
+        options: [
+            "Hyper Tool2 Markup Language",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 36,
+        question: "How do you create nested web pages in HTML?",
+        answer:  "Nested web pages basically mean a webpage within a webpage",
+        options: [
+            "Nested web pages basically mean a webpage within a webpage",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 37,
+        question: "What are the different types of headings in HTML?",
+        answer:  "Size",
+        options: [
+            "shape",
+            "fix",
+            "color",
+            "background-color"
+        ]
+    },
+    {
+        numb: 38,
+        question: "What is the alt attribute in HTML?",
+        answer:  "alternative",
+        options: [
+            "section",
+            "alternative",
+            "paragrahy",
+            "Hyperlinks and Text Markup Language"
+        ]
+    },
+    {
+        numb: 39,
+        question: "How do you add colour to the text in HTML?",
+        answer:  "<style = color: red;",
+        options: [
+            "font-sixe",
+            "style = color: red;",
+            "position:relative",
+            "align-item:center"
+        ]
+    },
+    {
+        numb: 40,
+        question: "What is HTML used for",
+        answer:  "Create the structure of a webpage",
+        options: [
+            "Create the structure of a webpage",
+            "For styling webpage",
+            "Make food",
+            "Is for adding functionality to webpage"
+        ]
+    },
+    {
+        numb: 41,
+        question: "How many HTML file can we have on our project",
+        answer:  "As many has required",
+        options: [
+            "Two",
+            "Not more than one",
+            "Not more than three",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 42,
+        question: "Can HTML document has style within",
+        answer:  "Yes, inline style can be done on html",
+        options: [
+            "Two",
+            "Yes, inline style can be done on html",
+            "No",
+            "Not sure"
+        ]
+    },
+    {
+        numb: 43,
+        question: "Which is the smallest header tag",
+        answer:  "<h1>",
+        options: [
+            "<h2>",
+            "<h3>",
+            "<p>",
+            "<h6>"
+        ]
+    },
+    {
+        numb: 44,
+        question: "<p>Can be used to hold paragrahy on website",
+        answer:  "Yes",
+        options: [
+            "No",
+            "Not sure",
+            "Sometimes",
+            "Yes"
+        ]
+    },
+    {
+        numb: 45,
+        question: "All project most have html file",
+        answer:  "Yes",
+        options: [
+            "No",
+            "Not sure",
+            "Yes",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 46,
+        question: "What is used to set width and font-size",
+        answer:  "Px and %",
+        options: [
+            "Px and %",
+            "*",
+            "+",
+            "-"
+        ]
+    },
+    {
+        numb: 47,
+        question: "There is no different between id and class",
+        answer:  "As many has required",
+        options: [
+            "Two",
+            "Not more than one",
+            "Not more than three",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 48,
+        question: "____________ is an example of attribute is HTML",
+        answer:  "Class",
+        options: [
+            "Class",
+            "<p>",
+            "<main>",
+            "<footer>"
+        ]
+    },
+    {
+        numb: 49,
+        question: "How many HTML file can we have on our project",
+        answer:  "As many has required",
+        options: [
+            "Two",
+            "Not more than one",
+            "Not more than three",
+            "As many has required"
+        ]
+    },
+    {
+        numb: 50,
+        question: "What is making web Standard?",
+        answer: "The World Wide Web Consortium" ,
+        options: [
+            "Google",
+            "Microsoft",
+            "Mozilla",
+            "The World Wide Web Consortium"
+        ]
+    },
+    ];
